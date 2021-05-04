7597d90ffea65fd4bd30a93c732636775d14d619
DassLopez <A01366815@itesm.mx>
2021-05-04 12:14:36 -0500

    Versión 1 Agregar línea y borrar línea

--- Programa.c
+++ Programa.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+// Cambio de A01366815 Dass López PARTICIPANTE
 int main(){
  int length;
  scanf("%i", &length);
  int office = pow(2,length);
  printf("%i",office);
- return 0;
+ // return 0; Borré estas líneas cómo SUPERVISOR cambio de A01366815
 }
